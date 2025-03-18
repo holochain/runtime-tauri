3fffa95cca9cb602d3721cc6fdb80ad99012561a
Matt Gabrenya <matt@buildyourweb.app>
2025-03-17 17:09:11 -0700

    chore: move uniffi-bindgen cli into its own crate to avoid rebuilding

--- crates/holochain-conductor-runtime-ffi/scripts/build-debug.sh
+++ crates/holochain-conductor-runtime-ffi/scripts/build-debug.sh
@@ -4,7 +4,7 @@ cargo ndk --manifest-path ./Cargo.toml -t arm64-v8a \
  -o ../tauri-plugin-holochain-service/android/src/main/jniLibs \
   build
 
-cargo run --bin uniffi-bindgen generate \
+cargo run -p uniffi-bindgen --release generate \
   --library ../../target/aarch64-linux-android/debug/libholochain_conductor_runtime_ffi.so \
   --crate holochain_conductor_runtime_ffi \
   --out-dir ../tauri-plugin-holochain-service/android/src/main/java/ \
