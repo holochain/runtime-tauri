1478ed028310b325414625930d1501dab70c5449
mattyg <matt@buildyourweb.app>
2025-03-26 10:04:08 -0700

    fix: version

--- crates/tauri-plugin-holochain-service-consumer/android/build.gradle.kts
+++ crates/tauri-plugin-holochain-service-consumer/android/build.gradle.kts
@@ -38,7 +38,7 @@ android {
 dependencies {
     // Subprojects
     implementation(project(":tauri-android"))
-    implementation("org.holochain.androidserviceruntime:holochain-service-client:7")
+    implementation("org.holochain.androidserviceruntime:holochain-service-client:0.0.10")
     // Kotlin
     implementation("androidx.core:core-ktx:1.7.0")
     implementation("androidx.appcompat:appcompat:1.6.0")
