7494134de556ca0838544d18b59e205a8e738039
Matt Gabrenya <matt@buildyourweb.app>
2025-03-27 16:26:06 -0700

    chore: rename package uniffi-bindgen -> uniffi-bindgen-cli for clarity

--- crates/holochain-conductor-runtime-ffi/scripts/build-single-target.sh
+++ crates/holochain-conductor-runtime-ffi/scripts/build-single-target.sh
@@ -6,7 +6,7 @@ cargo ndk --manifest-path ./Cargo.toml -t $TARGET \
  -o ../tauri-plugin-holochain-service/android/src/main/jniLibs \
   build --release
 
-cargo run -p uniffi-bindgen --release generate \
+cargo run -p uniffi-bindgen-cli --release generate \
   --library ../../target/$TARGET/release/libholochain_conductor_runtime_ffi.so \
   --crate holochain_conductor_runtime_ffi \
   --out-dir ../tauri-plugin-holochain-service/android/src/main/java/ \
--- crates/holochain-conductor-runtime-types-ffi/scripts/build-single-target.sh
+++ crates/holochain-conductor-runtime-types-ffi/scripts/build-single-target.sh
@@ -6,7 +6,7 @@ cargo ndk --manifest-path ./Cargo.toml -t $TARGET \
   -o ../tauri-plugin-holochain-service/android/src/main/jniLibs \
   build --release
 
-cargo run -p uniffi-bindgen --release generate \
+cargo run -p uniffi-bindgen-cli --release generate \
   --library ../../target/$TARGET/release/libholochain_conductor_runtime_types_ffi.so \
   --out-dir ../../libraries/holochain-service-client/src/main/java/ \
   --language kotlin
