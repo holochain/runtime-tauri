346bc65e47fdb9c9f6ede9cd323400095c6c27bf
Matt Gabrenya <matt@buildyourweb.app>
2025-04-11 13:48:32 -0700

    chore: clippy

--- crates/holochain-conductor-runtime/src/runtime.rs
+++ crates/holochain-conductor-runtime/src/runtime.rs
@@ -181,8 +181,8 @@ impl Runtime {
     /// Check if app is installed, if not install it, then optionally enable it.
     /// Then ensure there is an app websocket and authentication for it.
     ///
-    /// If an app is already installed, it will not be enabled. It is only enabled after a successful install. 
-    /// The reasoning is that if an app is disabled after that point, 
+    /// If an app is already installed, it will not be enabled. It is only enabled after a successful install.
+    /// The reasoning is that if an app is disabled after that point,
     /// it is assumed to have been manually disabled in the admin interface, which we don't want to override.
     pub async fn setup_app(
         &self,
