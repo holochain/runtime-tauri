63e0a6580bb29b039756c4c7e63b04569147fb52
Matt Gabrenya <matt@buildyourweb.app>
2025-05-16 10:45:38 -0700

    feat: pass runtime network settings when starting runtime, allow distributor of android-service-runtime app to configure

--- crates/runtime/src/autostart.rs
+++ crates/runtime/src/autostart.rs
@@ -1,4 +1,4 @@
-use crate::{Persisted, RuntimeResult};
+use crate::{Persisted, RuntimeNetworkConfig, RuntimeResult};
 use serde::{Deserialize, Serialize};
 use std::path::PathBuf;
 use std::sync::{Arc, RwLock};
@@ -9,10 +9,20 @@ const PERSISTED_FILE_NAME: &str = "autostart_config.msgpack";
 /// Is Auto Start on Boot enabled?
 #[derive(Clone, Serialize, Deserialize, Debug, Default)]
 struct AutostartConfig {
-    enabled: bool,
+    /// Is autostart enabled
+    ///
+    /// To enable autostart, set this to `Some(config)`,
+    /// where `config` is the RuntimeNetworkConfig to apply when starting the runtime.
+    ///
+    /// To disable autostart, set this to `None`.
+    enabled_config: Option<RuntimeNetworkConfig>,
 }
 
 /// Manager for reading & writing persisted autostart config
+///
+/// Not this does *not* actually handle starting the runtime,
+/// but is purely a configuration, which the consumer of this
+/// crate can read during its boot process.
 #[derive(Clone)]
 pub struct AutostartConfigManager {
     autostart_config: Arc<RwLock<AutostartConfig>>,
@@ -20,6 +30,9 @@ pub struct AutostartConfigManager {
 }
 
 impl AutostartConfigManager {
+    /// Initialize the AutostartConfigManager
+    ///
+    /// Read an existing config from the persisted state, if it exists.
     pub fn new(data_root_path: PathBuf) -> RuntimeResult<Self> {
         let persisted_path = data_root_path.join(PERSISTED_FILE_NAME);
         let data = Self::read_from_file(persisted_path.clone())?.unwrap_or_default();
@@ -30,11 +43,12 @@ impl AutostartConfigManager {
         })
     }
 
-    pub fn enable(&self) -> RuntimeResult<()> {
+    /// Enable autostart with the specified network config
+    pub fn enable(&self, config: RuntimeNetworkConfig) -> RuntimeResult<()> {
         // Add the config to the in-memory data
         {
             let mut autostart_config = self.autostart_config.write().unwrap();
-            autostart_config.enabled = true;
+            autostart_config.enabled_config = Some(config);
         }
 
         // Save all authorizations to the filesystem
@@ -43,11 +57,12 @@ impl AutostartConfigManager {
         Ok(())
     }
 
+    /// Disable autostart
     pub fn disable(&self) -> RuntimeResult<()> {
         // Add the config to the in-memory data
         {
             let mut autostart_config = self.autostart_config.write().unwrap();
-            autostart_config.enabled = false;
+            autostart_config.enabled_config = None;
         }
 
         // Save all authorizations to the filesystem
@@ -56,10 +71,14 @@ impl AutostartConfigManager {
         Ok(())
     }
 
-    pub fn is_enabled(&self) -> RuntimeResult<bool> {
+    /// Get the autostart config
+    ///
+    /// If autostart is enabled, returns `Ok(Some(config))`.
+    /// If autostart is disabled, returns `Ok(None)`.
+    pub fn get_enabled_config(&self) -> RuntimeResult<Option<RuntimeNetworkConfig>> {
         let autostart_config = self.autostart_config.read().unwrap();
 
-        Ok(autostart_config.enabled)
+        Ok(autostart_config.enabled_config.clone())
     }
 }
 
@@ -76,9 +95,11 @@ impl Persisted<AutostartConfig> for AutostartConfigManager {
 #[cfg(test)]
 mod test {
     use super::{AutostartConfig, AutostartConfigManager, PERSISTED_FILE_NAME};
+    use crate::RuntimeNetworkConfig;
     use std::fs::{exists, File};
     use std::io::{Read, Write};
     use tempfile::tempdir;
+    use url2::Url2;
 
     #[test]
     fn authorize_saves_to_file() {
@@ -88,7 +109,8 @@ mod test {
 
         // Create new autostart config and enable
         let manager = AutostartConfigManager::new(tmp_dir.path().to_path_buf()).unwrap();
-        manager.enable().unwrap();
+        let config = RuntimeNetworkConfig::default();
+        manager.enable(config.clone()).unwrap();
 
         // Assert persisted file exists
         assert!(exists(manager.persisted_path.clone()).unwrap());
@@ -100,7 +122,7 @@ mod test {
         let decoded: AutostartConfig = serde_json::from_slice(encoded.as_slice()).unwrap();
 
         // Assert persisted file contains authorized app pair
-        assert!(decoded.enabled);
+        assert_eq!(decoded.enabled_config.unwrap(), config);
     }
 
     #[test]
@@ -113,10 +135,12 @@ mod test {
         let manager = AutostartConfigManager::new(tmp_dir.path().to_path_buf()).unwrap();
 
         // Assert app pair is not enabled
-        assert!(!manager.is_enabled().unwrap());
+        assert!(manager.get_enabled_config().unwrap().is_none());
 
         // Write autostart config to file
-        let autostart_config = AutostartConfig { enabled: true };
+        let autostart_config = AutostartConfig {
+            enabled_config: Some(RuntimeNetworkConfig::default()),
+        };
         let encoded = serde_json::to_string(&autostart_config).unwrap();
         {
             let mut file = File::create(path.clone()).unwrap();
@@ -127,7 +151,7 @@ mod test {
         let manager2 = AutostartConfigManager::new(tmp_dir.path().to_path_buf()).unwrap();
 
         // Assert autostart is enabled
-        assert!(manager2.is_enabled().unwrap())
+        assert!(manager2.get_enabled_config().unwrap().is_some())
     }
 
     #[test]
@@ -139,15 +163,19 @@ mod test {
         let manager = AutostartConfigManager::new(tmp_dir.path().to_path_buf()).unwrap();
 
         // Assert app pair is not enabled
-        assert!(!manager.is_enabled().unwrap());
+        assert!(manager.get_enabled_config().unwrap().is_none());
 
         // Enable
-        let _ = manager.enable();
-        assert!(manager.is_enabled().unwrap());
+        let config = RuntimeNetworkConfig {
+            bootstrap_url: Url2::parse("https://google.com"),
+            ..RuntimeNetworkConfig::default()
+        };
+        let _ = manager.enable(config.clone());
+        assert!(manager.get_enabled_config().unwrap().is_some());
 
-        // Assert new instance is enabled
+        // Assert new instance is enabled with matching config
         let manager2 = AutostartConfigManager::new(tmp_dir.path().to_path_buf()).unwrap();
-        assert!(manager2.is_enabled().unwrap());
+        assert_eq!(manager2.get_enabled_config().unwrap(), Some(config));
     }
 
     #[test]
@@ -159,18 +187,18 @@ mod test {
         let manager = AutostartConfigManager::new(tmp_dir.path().to_path_buf()).unwrap();
 
         // Assert app pair is not enabled
-        assert!(!manager.is_enabled().unwrap());
+        assert!(manager.get_enabled_config().unwrap().is_none());
 
         // Enable
-        let _ = manager.enable();
-        assert!(manager.is_enabled().unwrap());
+        let _ = manager.enable(RuntimeNetworkConfig::default());
+        assert!(manager.get_enabled_config().unwrap().is_some());
 
         // Disable
         let _ = manager.disable();
-        assert!(!manager.is_enabled().unwrap());
+        assert!(manager.get_enabled_config().unwrap().is_none());
 
         // Assert new instance is disabled
         let manager2 = AutostartConfigManager::new(tmp_dir.path().to_path_buf()).unwrap();
-        assert!(!manager2.is_enabled().unwrap());
+        assert!(manager2.get_enabled_config().unwrap().is_none());
     }
 }
