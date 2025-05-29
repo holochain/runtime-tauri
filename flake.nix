5f8d7b7acab4ec96bc7673d5ad09a02ec9bf835f
Matt Gabrenya <matt@buildyourweb.app>
2025-05-29 16:36:41 -0700

    build: revert to p2p-shipyard's nix flake

--- flake.nix
+++ flake.nix
@@ -1,95 +1,40 @@
 {
-  description = "Flake for Android + Holochain app development";
+  description = "Template for Holochain app development";
 
   inputs = {
-    holonix.url = "github:holochain/holonix?ref=main-0.4";
+    holonix.url = "github:holochain/holonix/main-0.5";
+    p2p-shipyard.url = "github:darksoil-studio/tauri-plugin-holochain/main-0.5";
+
     nixpkgs.follows = "holonix/nixpkgs";
-    flake-parts.follows = "holonix/flake-parts";
-    rust-overlay.follows = "holonix/rust-overlay";
-
-    android-nixpkgs = {
-      url = "github:tadfisher/android-nixpkgs";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    scaffolding.url = "github:darksoil-studio/scaffolding/main-0.5";
   };
-  outputs = inputs@{ flake-parts, nixpkgs, rust-overlay, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
+
+  outputs = inputs @ { ... }:
+    inputs.holonix.inputs.flake-parts.lib.mkFlake { inherit inputs; }
+    {
       systems = builtins.attrNames inputs.holonix.devShells;
-      perSystem = { inputs', config, pkgs, system, ... }: {
-        formatter = pkgs.nixpkgs-fmt;
 
-        devShells.default =
-          let
-            overlays = [ (import rust-overlay) ];
-            pkgs = import nixpkgs {
-              inherit system overlays;
-            };
+      perSystem =
+        { inputs', pkgs, system, ...}: {
+          devShells.default = pkgs.mkShell {
+            inputsFrom = [
+              inputs'.p2p-shipyard.devShells.holochainTauriDev
+              inputs'.scaffolding.devShells.synchronized-pnpm
+              inputs'.holonix.devShells.default
+            ];
 
-            rust = (pkgs.rust-bin.stable.latest.minimal.override
-              {
-                extensions = [ "clippy" "rustfmt" "rust-src" ];
-                targets = [
-                  "wasm32-unknown-unknown"
-                  "aarch64-linux-android"
-                  "x86_64-linux-android"
-                  "i686-linux-android"
-                  "armv7-linux-androideabi"
-                ];
-              });
-
-            android = inputs.android-nixpkgs.sdk.${system} (sdkPkgs: with sdkPkgs; [
-              cmdline-tools-latest
-              build-tools-34-0-0
-              platform-tools
-              platforms-android-34
-              ndk-27-2-12479018
-            ]);
-          in
-          pkgs.mkShell {
-            packages = [
-              rust
-            ] ++ (with inputs'.holonix.packages; [
-              # For the example-client-app
-              holochain
-              lair-keystore
-              hc-launch
-              hc-scaffold
-              hn-introspect
-            ]) ++ (with pkgs; [
-              nodejs_20 # For UI development
-              pnpm
-              binaryen # For WASM optimisation
-
-              # Android development
-              android
-              gradle
-              jdk17
-              cargo-ndk
-              gnumake
-            ]);
-
-            # Tauri
-            buildInputs = (with pkgs; [
-              glibc
-              libsoup
-              cairo
-              atk
-              webkitgtk_4_1
-              openssl
-              librsvg
-            ]);
-            nativeBuildInputs = (with pkgs; [
-              pkg-config
-            ]);
-
-            ANDROID_HOME = "${android}/share/android-sdk";
-            NDK_HOME = "${android}/share/android-sdk/ndk/27.2.12479018";
-            JAVA_HOME = pkgs.jdk17.home;
-
-            shellHook = ''
-              export PS1='\[\033[1;34m\][holonix-android:\w]\$\[\033[0m\] '
-            '';
           };
-      };
+          devShells.androidDev = pkgs.mkShell {
+            inputsFrom = [
+              inputs'.p2p-shipyard.devShells.holochainTauriAndroidDev
+              inputs'.scaffolding.devShells.synchronized-pnpm
+              inputs'.holonix.devShells.default
+            ];
+
+            packages = [
+              pkgs.cargo-ndk
+            ];
+          };
+        };
     };
 }
