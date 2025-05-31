bbbe305f6b7281265d3a4dcbdf2c23555afd0ad5
Matt Gabrenya <matt@buildyourweb.app>
2025-05-30 19:20:11 -0700

    revert: use p2p-shipyard's nix flake

--- flake.nix
+++ flake.nix
@@ -3,38 +3,27 @@
 
   inputs = {
     holonix.url = "github:holochain/holonix/main-0.5";
-    p2p-shipyard.url = "github:darksoil-studio/tauri-plugin-holochain/main-0.5";
 
     nixpkgs.follows = "holonix/nixpkgs";
-    scaffolding.url = "github:darksoil-studio/scaffolding/main-0.5";
+    flake-parts.follows = "holonix/flake-parts";
+
+    tauri-plugin-holochain.url = "github:darksoil-studio/tauri-plugin-holochain/main-0.5";
   };
 
-  outputs = inputs @ { ... }:
-    inputs.holonix.inputs.flake-parts.lib.mkFlake { inherit inputs; }
-    {
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
       systems = builtins.attrNames inputs.holonix.devShells;
-
-      perSystem =
-        { inputs', pkgs, system, ...}: {
-          devShells.default = pkgs.mkShell {
-            inputsFrom = [
-              inputs'.p2p-shipyard.devShells.holochainTauriDev
-              inputs'.scaffolding.devShells.synchronized-pnpm
-              inputs'.holonix.devShells.default
-            ];
-
-          };
-          devShells.androidDev = pkgs.mkShell {
-            inputsFrom = [
-              inputs'.p2p-shipyard.devShells.holochainTauriAndroidDev
-              inputs'.scaffolding.devShells.synchronized-pnpm
-              inputs'.holonix.devShells.default
-            ];
-
-            packages = [
-              pkgs.cargo-ndk
-            ];
-          };
+      perSystem = { inputs', config, pkgs, system, ... }: {
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [ 
+            inputs'.tauri-plugin-holochain.devShells.holochainTauriAndroidDev 
+            inputs'.holonix.devShells.default
+          ];
+          
+          packages = [
+            pkgs.cargo-ndk
+          ];
         };
+      };
     };
 }
